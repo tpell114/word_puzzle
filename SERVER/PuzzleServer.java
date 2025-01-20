80b83dae2127330246f9c7a13c517b7d4824da05
Juan Diego Serrato <jserr894@mtroyal.ca>
2025-01-19 21:24:51 -0700

    more work done on server for single client

--- SERVER/PuzzleServer.java
+++ SERVER/PuzzleServer.java
@@ -47,6 +47,19 @@ public class PuzzleServer
             {
                 System.out.println("Listening for incoming requests...");
                 Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected.");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintStream out = new PrintStream(clientSocket.getOutputStream());
+
+                String inputMsg;
+                while((inputMsg = in.readLine()) != null)
+                {
+                    System.out.println("Received:" + inputMsg);
+                    byte cmdCode = parseCommand(inputMsg);
+                    handleClientReq(cmdCode, inputMsg, out);
+                }
+
             } 
             catch (SocketException e) 
             {
@@ -56,6 +69,73 @@ public class PuzzleServer
             {
                 e.printStackTrace();
             }
+        }
+    }
+
+
+    private byte parseCommand(String message)
+    {
+        String[] parts = message.split(" ", 2);
+        try
+        {
+            return (byte) Integer.parseInt(parts[0], 16);  
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Invalid cmd format" + parts[0]);
+            return -1;
+        }
+    }
+
+    private void handleClientReq(byte cmdCode, String message, PrintStream out)
+    {
+        switch (cmdCode)
+        {
+            case ProtocolConstants.CMD_EXIT:
+            //
+            break;
+
+            case ProtocolConstants.CMD_SIGN_IN:
+            //
+            break;
+
+            case ProtocolConstants.CMD_LEVEL_SET:
+            //
+            break;
+
+            case ProtocolConstants.CMD_SUBMIT_GUESS:
+            //
+            break;
+            
+            case ProtocolConstants.CMD_CHECK_SCORE:
+            //
+            break;
+
+            case ProtocolConstants.CMD_REQ_NEW_GAME:
+            //
+            break;
+
+            case ProtocolConstants.CMD_ABORT_GAME:
+            //
+            break;
+
+            case ProtocolConstants.CMD_CHECK_IF_WORD_EXISTS:
+            //
+            break;
+
+            case ProtocolConstants.CMD_ADD_WORD:
+            //
+            break;
+
+            case ProtocolConstants.CMD_REMOVE_WORD:
+            //
+            break;
+
+            default:
+            //send unknown command recieved message
+            break;
+
+        
         }
     }
 }
